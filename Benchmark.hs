aaadf9cb70534dccb0baa2cba18bc54f912f0a37
Samuel Gélineau <gelisam+github@gmail.com>
2020-12-28 22:50:53 -0500

    remove unused imports

--- Benchmark.hs
+++ Benchmark.hs
@@ -1,37 +1,21 @@
 #!/usr/bin/env cabal
 {- cabal:
 build-depends: base
-             , bytestring
              , directory
              , filepath
-             , lens
-             , lens-aeson
-             , stm
              , text
              , time
              , typed-process
-             , yaml
 -}
 {-# LANGUAGE LambdaCase, OverloadedStrings, ScopedTypeVariables #-}
 
-import Control.Concurrent.STM (STM)
-import Control.Lens ((&), (.~))
-import Data.ByteString.Lazy (ByteString)
-import Data.Text.Lazy (Text)
+import Data.Function ((&))
 import Data.Time.Clock
 import Data.Foldable (for_)
-import System.Directory (createDirectory, removeDirectoryRecursive)
 import System.Environment (getArgs, getProgName)
 import System.Exit (ExitCode(ExitFailure, ExitSuccess))
 import System.FilePath ((<.>), (</>))
 import System.Process.Typed (ProcessConfig)
-import qualified Control.Concurrent.STM as STM
-import qualified Data.ByteString.Lazy.Char8 as ByteString
-import qualified Data.List as List
-import qualified Data.Maybe as List (mapMaybe)
-import qualified Data.Text.Lazy as Text
-import qualified Data.Text.Lazy.Encoding as Text
-import qualified Data.Text.Lazy.IO as Text
 import qualified System.Directory as FilePath
 import qualified System.Process.Typed as Process
 
@@ -54,8 +38,8 @@ main = do
 
       for_ caseSubdirs $ \caseSubdir -> do
         -- "no-magic"
-        let caseName :: Text
-            caseName = Text.pack caseSubdir
+        let caseName :: String
+            caseName = caseSubdir
 
         -- "/.../benchmark-cases/no-magic"
         let workingDir :: FilePath
